a952d88fcf56a35aaae1b8685a74018935c91fc7
adamiaonr <adamiaonr@gmail.com>
2022-06-02 20:48:24 +0100

    code for Q3 and Q4

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -1,10 +1,10 @@
 import argparse
 import os
 import pickle
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
-
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -17,10 +17,15 @@ def run(data_path):
     X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
     rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_valid)
 
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+    # start an mlflow experiment run run : what does mlflow keep track of?
+    with mlflow.start_run():
+      # this enables automatic logging of experiment data
+      mlflow.autolog()
+      rf.fit(X_train, y_train)
+      y_pred = rf.predict(X_valid)
+
+      rmse = mean_squared_error(y_valid, y_pred, squared=False)
 
 
 if __name__ == '__main__':
@@ -32,5 +37,11 @@ if __name__ == '__main__':
         help="the location where the processed NYC taxi trip data was saved."
     )
     args = parser.parse_args()
+
+    # set uri of backend used to save ml experiment data
+    #mlflow.set_tracking_uri("sqlite:///../../data/mlflow/mlflow.db")
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    # set name of experiment (if non-existent in backend, mlflow creates it)
+    mlflow.set_experiment("homework_2_experiment")
 
     run(args.data_path)
