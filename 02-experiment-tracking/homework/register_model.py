36adf2aef79974d12f5b322248ce136e3e040a4f
adamiaonr <adamiaonr@gmail.com>
2022-06-02 21:27:16 +0100

    code for Q6

--- 02-experiment-tracking/homework/register_model.py
+++ 02-experiment-tracking/homework/register_model.py
@@ -65,11 +65,15 @@ def run(data_path, log_top):
 
     # select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.test_rmse ASC"]
+    )[0]
 
     # register the best model
-    # mlflow.register_model( ... )
-
+    mlflow.register_model(model_uri="runs:/{run_id}/model".format(run_id = best_run.info.run_id), name="nyc-green-taxi-rf-regressor")
 
 if __name__ == '__main__':
 
